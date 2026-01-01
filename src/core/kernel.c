c48d4c20d1b410bb12eadc4d858078e3e8c5e558
yanderemia <maty12czsk@gmail.com>
2026-01-01 04:22:26 +0100

    Auto-update 2026-01-01 04:22:26

--- src/core/kernel.c
+++ src/core/kernel.c
@@ -1,9 +1,26 @@
 #include <stdint.h>
 #include "../../inc/core/terminal.h"
+void dispatcher(const char *cmd);
+
 void kernel_main(void) {
     char cmd[256];
     while(1) {
+        print("NoctisOS> ");
         read(cmd, sizeof(cmd));
+        printc('\n');
+        dispatcher(cmd);
     }
 }
 
+// Test dispatcher for now tmrw gonna add it to /src/core/dispatcher.c
+#include "../../inc/utils/kstrcmp.h"
+
+void dispatcher(const char *cmd) {
+    if (kstrcmp(cmd, "help") == 0) {
+        print("help - shows this page\n");
+        return;
+    }
+    print("Invalid Command: ");
+    print(cmd);
+    printc('\n');
+}
