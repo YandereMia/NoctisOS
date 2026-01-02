ade4ed154f6e71a02c804efb7bb1ebf9f21f9ffe
yanderemia <maty12czsk@gmail.com>
2026-01-02 11:19:18 +0100

    Auto-update 2026-01-02 11:19:18

--- src/core/dispatcher.c
+++ src/core/dispatcher.c
@@ -16,8 +16,23 @@ void dispatcher(const char *cmd) {
         banner();
         return;
     }
+    if (kstrcmp(cmd, "test") == 0) {
+        // All test commands go here:
+        // return has to be after every command because if there wasnt return it could lead to unexpected behavior such as executing the command
+        // and event if it executes it goes deeper and prints that its Invalid Command even when it isnt
+        return;
+    }
     print("Invalid Command: ");
     print(cmd);
     printc('\n');
     return;
 }
+
+/*TODO
+add more commands
+make them separated from dispatcher in /commands/cmd_*.c to add some modularization and to prevent any bugs
+and for easier debugging
+commands todo today:
+manpage and changelog which will print info about new versions
+implement full/partial string.h lib add some fine touches to the OS and add customization along side with Hardcoded login root:root 
+and maybe fake booting during start up and have atleast partial functioning RAMFS*/
